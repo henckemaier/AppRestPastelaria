7ee589a130379131f1b43f7da27dea9ba7b7aa6d
User-Wellengton <wellengtonjm@gmail.com>
2022-09-28 20:39:34 -0300

    fghgfhgfh

---
+++ mod_index/index.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+bp_index = Blueprint('index', __name__, url_prefix="/", template_folder='templates')
+
+''' rotas dos formulários '''
+
+@bp_index.route('/')
+def formListaIndex():
+    return render_template('formIndex.html'), 200
