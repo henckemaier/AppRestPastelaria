08eed8672bffc243a7ee85a8b952507cac83fcdf
User-Wellengton <wellengtonjm@gmail.com>
2022-10-26 20:52:04 -0300

    ytyugyucfyyi

--- mod_funcionario/funcionario.py
+++ mod_funcionario/funcionario.py
@@ -1,13 +1,58 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+import requests
+from funcoes import Funcoes
+
+#senha = request.form['senha']
+senha = Funcoes.cifraSenha(request.form['senha'])
 
 bp_funcionario = Blueprint('funcionario', __name__, url_prefix="/funcionario", template_folder='templates')
+
+''' endereços do endpoint '''
+urlApiFuncionarios = "http://localhost:8000/funcionario/"
+urlApiFuncionario = "http://localhost:8000/funcionario/%s"
+headers = {'x-token':'abcBolinhasToken','x-key':'abcBolinhasKey'}
 
 ''' rotas dos formulários '''
 
 @bp_funcionario.route('/', methods=['GET', 'POST'])
 def formListaFuncionario():
-    return render_template('formListaFuncionario.html')
+    try:
+        response = requests.get(urlApiFuncionarios, headers=headers)
+        result = response.json()
+        if (response.status_code != 200):
+            raise Exception(result)
+        return render_template('formListaFuncionario.html', result=result)
 
-@bp_funcionario.route('/form-funcionario/', methods=['GET'])
+    except Exception as e:
+        return render_template('formListaFuncionario.html', erro=e)
+
+
+@bp_funcionario.route('/form-funcionario/', methods=['POST'])
 def formFuncionario():
     return render_template('formFuncionario.html')
+
+''' rotas de função '''
+
+@bp_funcionario.route('/insert', methods=['POST'])
+def insert():
+    try:
+        # dados enviados via FORM
+        id_funcionario = request.form['id']
+        nome = request.form['nome']
+        matricula = request.form['matricula']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        grupo = request.form['grupo']
+        senha = request.form['senha']
+
+        # monta o JSON para envio a API
+        payload = {'id_funcionario': id_funcionario, 'nome': nome, 'matricula': matricula,'cpf': cpf, 'telefone': telefone, 'grupo': grupo, 'senha': senha}
+
+        # executa o verbo POST da API e armazena seu retorno
+        response = requests.post(urlApiFuncionarios, headers=headers, json=payload)
+        result = response.json()
+
+        return render_template('formListaFuncionario.html', msg=result)
+
+    except Exception as e:
+        return render_template('formListaFuncionario.html', msgErro=e)
--- mod_produto/produto.py
+++ mod_produto/produto.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 
+import base64
+
 bp_produto = Blueprint('produto', __name__, url_prefix="/produto", template_folder='templates')
 
 ''' rotas dos formulários '''
@@ -7,3 +9,28 @@ bp_produto = Blueprint('produto', __name__, url_prefix="/produto", template_fold
 @bp_produto.route('/')
 def formListaProduto():
     return render_template('formListaProduto.html'), 200
+
+@bp_produto.route('/insert', methods=['POST'])
+def insert():
+    try:
+        # dados enviados via FORM
+        id_produto = request.form['id_produto']
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        #foto = request.form['foto']
+        valor_unitario = request.form['valor_unitario’]
+
+        # converte a foto em base64
+        foto = "data:" + request.files['foto'].content_type + ";base64," + str(base64.b64encode(request.files['foto'].read()), "utf-8")
+
+        # monta o JSON para envio a API
+        payload = {'id_produto': id_produto, 'nome': nome, 'descricao': descricao, 'foto': foto, 'valor_unitario': valor_unitario}
+
+        # executa o verbo POST da API e armazena seu retorno
+        response = requests.post(urlApiProdutos, headers=headers, json=payload)
+        result = response.json()
+
+        return render_template('formListaProduto.html', msg=result)
+
+    except Exception as e:
+        return render_template('formListaProduto.html', msgErro=e)
